a90b54024432b0deb93cb6d6a47f991e440c6a5d
Cris <crisheight@gmail.com>
2025-12-17 00:13:52 -0600

    Initial project setup

---
+++ gallery-api/src/test/java/com/github/crisheight/gallery_api/GalleryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.crisheight.gallery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
