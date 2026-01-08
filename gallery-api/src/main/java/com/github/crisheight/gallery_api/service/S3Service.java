a28dcf37f58d125484421dc5474ff08be3c98278
Cris <crisheight@gmail.com>
2026-01-08 16:27:46 -0600

    Refactor: keep all file names but assign unique UUIDs.

--- gallery-api/src/main/java/com/github/crisheight/gallery_api/service/S3Service.java
+++ gallery-api/src/main/java/com/github/crisheight/gallery_api/service/S3Service.java
@@ -1,34 +1,28 @@
 package com.github.crisheight.gallery_api.service;
 
+import io.awspring.cloud.s3.S3Resource;
 import io.awspring.cloud.s3.S3Template;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.time.Duration;
-import java.util.UUID;
 
 @Service
 public class S3Service {
 
     private final S3Template s3Template;
 
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucketName;
+
     public S3Service(S3Template s3Template) {
         this.s3Template = s3Template;
     }
 
-    public String uploadFile(String bucketName, MultipartFile file) throws IOException {
-        String key = UUID.randomUUID() + "-" + file.getOriginalFilename();
 
-        try (InputStream inputStream = file.getInputStream()) {
-            s3Template.upload(bucketName, key, inputStream);
-        }
-
-        return key;
-    }
-
-    public String createPresignedGetUrl(String bucketName, String key) {
-        return s3Template.createSignedGetURL(bucketName, key, Duration.ofMinutes(60)).toString();
+    public String uploadFile(String key, MultipartFile file) throws IOException {
+        S3Resource resource = s3Template.store(bucketName, key, file.getResource());
+        return resource.getURL().toString();
     }
 }
