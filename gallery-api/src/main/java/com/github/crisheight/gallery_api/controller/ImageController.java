7b4326ccd4fbf0efec689c7218bac9af61609121
Cris <crisheight@gmail.com>
2026-01-08 16:25:42 -0600

    Refactor: simplify controller so changes affect it less -> logic resides in ImageService.java now. The controller calls the service.

--- gallery-api/src/main/java/com/github/crisheight/gallery_api/controller/ImageController.java
+++ gallery-api/src/main/java/com/github/crisheight/gallery_api/controller/ImageController.java
@@ -1,64 +1,33 @@
 package com.github.crisheight.gallery_api.controller;
 
 import com.github.crisheight.gallery_api.model.Image;
-import com.github.crisheight.gallery_api.repository.ImageRepository;
-import com.github.crisheight.gallery_api.service.S3Service;
-import org.springframework.beans.factory.annotation.Value;
+import com.github.crisheight.gallery_api.service.ImageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/images")
+@CrossOrigin(origins = "*")
 public class ImageController {
 
-    private final S3Service s3Service;
-    private final ImageRepository imageRepository;
-    private final String bucketName;
+    private final ImageService imageService;
 
-    // Injecting the Service, ImageRepository, and the bucket name from properties
-    public ImageController(S3Service s3Service,
-                           ImageRepository imageRepository,
-                           @Value("${spring.cloud.aws.s3.bucket}") String bucketName) {
-        this.s3Service = s3Service;
-        this.imageRepository = imageRepository;
-        this.bucketName = bucketName;
-    }
-
-    @PostMapping("/upload")
-    public ResponseEntity<Map<String, String>> uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
-        Map<String, String> response = new HashMap<>();
-
-        // Upload file; get key
-        String key = s3Service.uploadFile(bucketName, file);
-
-        // Generate temp url
-        String signedUrl = s3Service.createPresignedGetUrl(bucketName, key);
-
-        // Save the metadata
-        Image image = new Image(file.getOriginalFilename(), signedUrl, key);
-        imageRepository.save(image); // SQL insert
-
-        response.put("url", signedUrl);
-        response.put("message", "Upload successful");
-        return ResponseEntity.ok(response);
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
     }
 
     @GetMapping
     public List<Image> getAllImages() {
-        List<Image> images = imageRepository.findAll();
-
-        // Loop and refresh the signed URLs
-        for (Image image : images) {
-            String signedUrl = s3Service.createPresignedGetUrl(bucketName, image.getS3Key());
-            image.setUrl(signedUrl); // Update the object in memory before returning (don't save to DB)
-        }
-
-        return images;
+        return imageService.getAllImages();
     }
-} // End ImageController
+
+    @PostMapping("/upload")
+    public ResponseEntity<Image> uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
+        Image savedImage = imageService.uploadImage(file);
+        return ResponseEntity.ok(savedImage);
+    }
+}
