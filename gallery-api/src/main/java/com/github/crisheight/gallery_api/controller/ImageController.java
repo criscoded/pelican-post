14a90b938548225b837473e56c0541f14c46a1ff
Cris <cristofercodes@gmail.com>
2026-08-16 21:20:58 -0500

    Added mapping for user images and mapping for getting images from id

--- gallery-api/src/main/java/com/github/crisheight/gallery_api/controller/ImageController.java
+++ gallery-api/src/main/java/com/github/crisheight/gallery_api/controller/ImageController.java
@@ -1,8 +1,10 @@
 package com.github.crisheight.gallery_api.controller;
 
 import com.github.crisheight.gallery_api.model.Image;
+import com.github.crisheight.gallery_api.security.AppUserPrincipal;
 import com.github.crisheight.gallery_api.service.ImageService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,21 +22,29 @@ public class ImageController {
     }
 
     @GetMapping
-    public List<Image> getAllImages() {
-        return imageService.getAllImages();
+    public List<Image> getMyImages(@AuthenticationPrincipal AppUserPrincipal principal) {
+        return imageService.getAllImages(principal.getUserId());
+    }
+
+    @GetMapping("/{id}")
+    public Image getImage(@PathVariable Long id, @AuthenticationPrincipal AppUserPrincipal principal) {
+        return imageService.getImage(id, principal.getUserId());
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<Image> uploadImage(@RequestParam("file") MultipartFile file, 
+    public ResponseEntity<Image> uploadImage(@RequestParam("file") MultipartFile file,
                                              @RequestParam(value = "note", required = false) String note,
-                                             @RequestParam(value = "theme", required = false) String theme) throws IOException {
-        Image savedImage = imageService.uploadImage(file, note, theme);
+                                             @RequestParam(value = "theme", required = false) String theme,
+                                             @AuthenticationPrincipal AppUserPrincipal principal) throws IOException {
+        Image savedImage = imageService.uploadImage(principal.getUserId(), file, note, theme);
         return ResponseEntity.ok(savedImage);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteImage(@PathVariable Long id) throws IOException {
-        imageService.deleteImage(id);
+    public ResponseEntity<Void> deleteImage(@PathVariable Long id,
+                                            @AuthenticationPrincipal AppUserPrincipal principal) throws IOException {
+        imageService.deleteImage(id, principal.getUserId());
         return ResponseEntity.noContent().build(); // Returns HTTP 204
     }
-}
+
+} // End ImageController
