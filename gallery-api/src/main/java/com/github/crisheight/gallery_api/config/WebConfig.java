9ae0d775d27c19b4a264945aaa35c2f016222586
Cris <cristofercodes@gmail.com>
2026-08-16 21:24:16 -0500

    restricted cors mapping and allowed headers

--- gallery-api/src/main/java/com/github/crisheight/gallery_api/config/WebConfig.java
+++ gallery-api/src/main/java/com/github/crisheight/gallery_api/config/WebConfig.java
@@ -14,10 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins(allowedOrigins.split(","))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin")
+                .maxAge(3600);
     }
-}
+} // End WebConfig
